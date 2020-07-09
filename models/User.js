1b2ceb57cd0cd5316ba2f23cd69027f4641b47f1
Matthew Jiao <jiaomatthew222@gmail.com>
2020-07-08 21:39:07 -0400

    new schema for online count

--- models/User.js
+++ models/User.js
@@ -45,6 +45,10 @@ const UserSchema = new mongoose.Schema({
     code:{
         type: Number,
         default: 0
+    },
+    status:{
+        type: String,
+        default: "offline"
     }
 
 })
