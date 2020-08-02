0bc523bcd7c0c714710e0b3c754e09708ef33a9c
Nicholas <tao.nicholas1@gmail.com>
2020-08-02 16:25:17 -0400

    more merge conflict fixes

--- dashboard-styling/js/matching.js
+++ dashboard-styling/js/matching.js
@@ -59,11 +59,7 @@ function startBtnClicked() {
             clearInterval(periodicPing);
             audio.play();
             window.location.replace(
-<<<<<<< HEAD
               "https://chat.wavechat.tech/?room=" + d.roomLink
-=======
-              "https://chat.wave.tech/?room=" + d.roomLink
->>>>>>> 9d5246e2145280a3a000d1d769d882dd94d71322
             );
           }
         });
